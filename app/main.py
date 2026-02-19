01d2e4d9a6f38ca6898aecff6f76889b057a080b
akshat45 <gargakshat952@gmail.com>
2026-02-19 11:21:31 +0530

    modified to handle more users

--- app/main.py
+++ app/main.py
@@ -57,5 +57,5 @@ if __name__ == "__main__":
         "app.main:app",
         host=settings.HOST,
         port=settings.PORT,
-        reload=True
+        reload=False  # Never use reload=True in production (disables multi-threading)
     )
